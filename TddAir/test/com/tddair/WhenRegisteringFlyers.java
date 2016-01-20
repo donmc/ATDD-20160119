49e5e96657289e91f167202f5a950d01c83ad282
Kalpesh <customer@10.120.0.137>
2016-01-20 12:07:07 -0600

    Added Behavior tests for Registering Flyer.

---
+++ TddAir/test/com/tddair/WhenRegisteringFlyers.java
@@ -0,0 +1,53 @@
+package com.tddair;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class WhenRegisteringFlyers 
+{
+	private Member member = null;
+
+	@Before
+	public void setup()
+	{
+		String username = "martym";
+		String email = "martymcfly@future.com";
+		TddAirApplication tddAirApp = new TddAirApplication();
+		tddAirApp.registerAsMember(username, email);
+		
+		member = tddAirApp.lookUp(username);
+	}
+	
+	@Test
+	public void shouldSaveAsMember() 
+	{
+		assertNotNull(member);
+		assertEquals("martym", member.getUserName());
+	}
+
+	@Ignore
+	@Test
+	public void shouldHaveRedStatus() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Ignore
+	@Test
+	public void shouldHave10000BonusMilesForBalance() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Ignore
+	@Test
+	public void shouldHave0YTD() 
+	{
+		fail("Not yet implemented");
+	}
+}
