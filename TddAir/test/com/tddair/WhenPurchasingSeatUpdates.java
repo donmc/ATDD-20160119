0016f071eed22e4bd5c282ea95c673a2253c7bff
Kaine <customer@10.120.0.132>
2016-01-21 13:21:41 -0600

    test: whoops

---
+++ TddAir/test/com/tddair/WhenPurchasingSeatUpdates.java
@@ -0,0 +1,39 @@
+package com.tddair;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhenPurchasingSeatUpdates {
+
+	@Before
+	public void setup() {
+		//TODO: Something
+	}
+	
+	@Test
+	public void shouldPurchaseUpgradesWithBalance() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shouldPurchaseThreeUpgradesWithBalance() {
+		
+	}
+	
+	@Test
+	public void shouldNotPurchaseUpdatesWithInsufficientBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void shouldPurchaseUpgradeWithCC() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shouldNotPurchaseUpgradeWithInvalidCC() {
+		fail("Not yet implemented");
+	}
+}
